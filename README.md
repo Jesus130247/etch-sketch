2a44a6f0fc1bc420e194fc24c7779c170cc7ae1b
Jesus130247 <leonardo.monty@hotmail.com>
2024-01-23 15:49:06 +1100

    rough framework

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # etch-sketch
+
+Project: Etch-a-Sketch
+
+- Creating a webpage with a 16x16 grid of square divs
+- create divs using JS
+- use another container div to put square in
+-- use flexbox oooooop
---
+++ javascript.js
@@ -0,0 +1,5 @@
+// need to use js to create a div "box"
+// need to use flexbox to align the box in the correct position
+// need to use js to create a 16x16 grid. total of 256 boxs
+
+// then need to use js to have hover effect on these boxs
