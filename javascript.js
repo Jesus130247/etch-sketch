696d4b0c42bb57329e34de4dbe92f7ed0813c443
Jesus130247 <leonardo.monty@hotmail.com>
2024-01-24 12:46:49 +1100

    16x16 grid HARD CODE This isnt exactly hard-code, but it only works for a 16x16 grid if you want to do a different size, then it wouldn't work. i will work on that next, but i want to see if everything else works first

--- javascript.js
+++ javascript.js
@@ -1,23 +1,38 @@
 function createGrid(width, height) {
-    const ROW = document.querySelector('.row')
+    const ROW = document.querySelector('.container')
     let divRow = undefined
-    const COLUMN = document.querySelector('.column')
-    let divColumn = undefined
+    // const COLUMN = document.querySelector('.column')
+    // let divColumn = undefined
 
     for (let i = 0; i < width; i++) {
-        divRow = document.createElement('div')
-        ROW.appendChild(divRow)
+        for (let j = 0; j < height; j++) {
+            divRow = document.createElement('div')
+            ROW.appendChild(divRow)
+            console.log(i)
+            console.log(j)
+        }
     }
-    for (let i = 0; i < height; i++) {
-        divColumn = document.createElement('div')
-        COLUMN.appendChild(divColumn)
-    }
+
 
 }
 
-createGrid(3,3)
+createGrid(16,16)
 
+// I have a big box called container
+// within container, i currently have 2 containers.
+// container 1 = ROW: is my row box
+// container 2 = COLUM: is my column box
+// so the issue is, even tho i can create infinte boxs to put into these containers, they only ever exist within 2 containers
 
+// POSSIBLE SOLUTIONS
+
+// ------- I can try and make each of those containers a fixed size, and make it flex wrap. but if thats the case, why don't i try just putting 256 boxs
+// into the original containers. Ill try that
+// THIS WORKS --- negative: HARD CODE, does not allow for any lee-way
+
+// ------- I can try adding aditional containers, and then to those adding 16 boxs. For example; I have CONTAINER, to CONTAINER i add COLUMN_1, 
+// to COLUMN_1 i add 16 boxs. then to CONTAINER i add COLUMN_2, and to COLUMN_2 i add 16 boxs.
+// benefits of this: it means i could add COLUMN_n to CONTAINER, and to each COLUMN i could add n boxs.
 
 // need to use js to create a div "box"
 // need to use flexbox to align the box in the correct position
