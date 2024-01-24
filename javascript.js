d2bc6c8f52aa8546a4839231c3f2b1f682959f7b
Jesus130247 <leonardo.monty@hotmail.com>
2024-01-24 12:02:51 +1100

    Function creates boxs These boxs are only in the first row. and only create A+B this is instead of AxB thus a row and not a box.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,24 @@
+function createGrid(width, height) {
+    const ROW = document.querySelector('.row')
+    let divRow = undefined
+    const COLUMN = document.querySelector('.column')
+    let divColumn = undefined
+
+    for (let i = 0; i < width; i++) {
+        divRow = document.createElement('div')
+        ROW.appendChild(divRow)
+    }
+    for (let i = 0; i < height; i++) {
+        divColumn = document.createElement('div')
+        COLUMN.appendChild(divColumn)
+    }
+
+}
+
+createGrid(3,3)
+
+
+
 // need to use js to create a div "box"
 // need to use flexbox to align the box in the correct position
 // need to use js to create a 16x16 grid. total of 256 boxs
